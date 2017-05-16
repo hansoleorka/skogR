91bb039e47dc264cc6bedc67a3f09eeb6b7b0f49
kmhaug <marius.hauglin@nmbu.no>
2017-05-16 12:32:54 +0200

    Endret slik at den laveste av de to mulige løsningene alltid blir valgt.

--- R/heightFromVolume.R
+++ R/heightFromVolume.R
@@ -5,8 +5,8 @@
 #' @param vol numerical vector; single tree volume. dm3.
 #' @param d numerical vector; diameters in cm.
 #' @param sp numerical vector; species.
-#' @param interval bounds of the possible height values. Used in the optimization function.
-#'
+#' @param interval numeric vector; default lower/upper bound for optim
+
 #' @return a vector of the same length as the input with estimated heights.
 #'
 #'
@@ -14,41 +14,49 @@
 #' @export
 
 
-heightFromVolume<-function(vol,d,sp,interval=c(2,60)){
+heightFromVolume<-function(vol,d,sp,interval=c(0,45)){
 	
 	esth<-rep(NA,length(vol))
 
 	for (i in 1:length(vol)){	
+		
 
 	# function to minimize
 	if (sp[i] == 1 & d[i] > 13) {
 		minfun<-function(h) abs( vol[i] - (10.14 + 0.0124 * d[i] * d[i] * h + 0.03117 * d[i] * h * h - 0.36381 * h * h + 0.28578 * d[i] * h) )
+		# interval<-c(,) # lower and upper bound of h for optimization
 	}
 	
 	if (sp[i] == 1 & d[i] > 10  & d[i] <= 13){
 		minfun<-function(h) abs( vol[i] - (-31.57 + 0.0016 * d[i] * h * h + 0.0186 * h * h + 0.63 * d[i] * h - 2.34 * h + 3.2 * d[i]) )
+		# interval<-c(,)	
 	}
 	
 	if (sp[i] == 1 & d[i] <= 10) {
 		minfun<-function(h) abs( vol[i] - (0.52 + 0.02403 * d[i] * d[i] * h + 0.01463 * d[i] * h * h - 0.10983 * h * h + 0.15195 * d[i] * h) )
+		#interval<-c(0,25)	
 	}
 	
 	if (sp[i] == 2 & d[i] <= 11) {
 		minfun<-function(h) abs( vol[i] - (2.912 + 0.039994 * d[i] * d[i] * h - 0.001091 * d[i] * h * h) )
+		# interval<-c(0,)	
 	}
 	
 	if (sp[i] == 2 & d[i] > 11){
 		minfun<-function(h) abs( vol[i] - (8.6524 + 0.076844 * d[i] * d[i] + 0.031573 * d[i] * d[i] * h) )
+		# interval<-c(,)	
 	}
 	
 	
 	if (sp[i] == 3){
 		minfun<-function(h) abs( vol[i] - (-1.25409 + 0.12739 * d[i] * d[i] + 0.03166 * d[i] * d[i] * h + 0.0009752 * d[i] * h * h - 0.01226 * h * h - 0.004214 * d[i] * d[i] * (1.046 * d[i])) )
+		# interval<-c(,)	
 	}
 	
 	
 	# optimization
-	esth[i]<-optimize(minfun,interval=interval)$minimum
+	esth_1<-optimize(minfun,interval=interval)$minimum
+	esth[i]<-optimize(minfun,interval=c(0,esth_1))$minimum
 	
 	}
 	
